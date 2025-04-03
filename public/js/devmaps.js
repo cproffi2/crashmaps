af09a6d3c3b5db76c9e455f5da5c94ac29502f9e
= <=>
2025-04-02 21:44:10 -0700

    nope

--- public/js/devmaps.js
+++ public/js/devmaps.js
@@ -1,4 +1,4 @@
-import { MarkerClusterer } from "@googlemaps/markerclusterer";
+//import { MarkerClusterer } from "@googlemaps/markerclusterer";
 
 // Declare markers and map globally
 let markers = [];
@@ -116,7 +116,7 @@ async function updateMapWithCrashData(crashData) {
         markers.push(marker);
     });
 
-     new MarkerClusterer({markers, map})
+     //new MarkerClusterer({markers, map})
     console.log(`${crashData.length} markers added.`);
 }
 
